09c63e6e04bd8ca02ab3070d90c420157c3f2ebf
sefametin <sefametin68@gmail.com>
2024-03-02 02:41:37 +0300

    Homework1 over

--- marathon-Homework1/marathon-Homework1/src/App.java
+++ marathon-Homework1/marathon-Homework1/src/App.java
@@ -3,8 +3,22 @@ public class App {
         String names[] = { "Kadir", "Gokhan", "Hakan", "Suzan", "Pinar", "Mehmet", "Ali", "Emel", "Firat", "James",
                 "Jale", "Ersin", "Deniz", "Gözde", "Anil", "Burak" };
         int scores[] = { 341, 273, 278, 329, 445, 402, 388, 270, 243, 334, 412, 393, 299, 343, 317, 265 };
-        Marathon marathon = new Marathon(names, scores);
+
+        Marathon cuMarathon = new Marathon(names, scores);
         MarathonManager marathonManager = new MarathonManager();
-        System.out.println(marathonManager.first());
+
+        int first = marathonManager.first(cuMarathon);
+        System.out.println("Birinci: " + names[first] + " " + scores[first] + "'");
+        
+        int second = marathonManager.second(cuMarathon);
+        System.out.println("Ikinci:  " + names[second] + " " + scores[second] + "'");
+
+        int third = marathonManager.third(cuMarathon);
+        System.out.println("Ucuncu:  " + names[third] + " " + scores[third] + "'");
+
+        int[] numbers = marathonManager.studentPerScore(cuMarathon);
+        System.out.println("A --> " + numbers[0]);
+        System.out.println("B --> " + numbers[1]);
+        System.out.println("C --> " + numbers[2]);
     }
 }
--- marathon-Homework1/marathon-Homework1/src/MarathonManager.java
+++ marathon-Homework1/marathon-Homework1/src/MarathonManager.java
@@ -1,18 +1,89 @@
 public class MarathonManager {
 
-    public int first(Marathon marathon){
+    // birinci kosucunun arraydeki yerini donduren fonksiyon
+    public int first(Marathon marathon) {
         int[] scores = marathon.getScores();
-        int max = scores[0];
-        for(int score:scores){
-            if(score > max)
-                max = score;
+        int min = scores[0];
+        for (int score : scores) {  //1. olan kisinin skorunu bulur
+            if (score < min)
+                min = score;
         }
 
-        for (int i=0 ; i<16 ; i++){
-            if(scores[i]==max){
-                return i;
+        int i = 0;
+        for (i = 0; i < 16; i++) {  //1. olan kisinin dizideki konumunu bulur
+            if (scores[i] == min) {
+                break;
             }
         }
-        return -1;
+        return i;
+    }
+
+    // ikinci kosucunun arraydeki yerini donduren fonksiyon
+    public int second(Marathon marathon) {
+        int[] scores = marathon.getScores();
+        int min = scores[0];
+        MarathonManager marathonManager = new MarathonManager();
+        int first = marathonManager.first(marathon);
+
+        for (int i = 0; i < 16; i++) {  //2. olan kisinin skorunu bulur
+            if (i == first) {
+                continue;
+            }
+
+            else if (scores[i] < min)
+                min = scores[i];
+        }
+
+        int i = 0;
+        for (i = 0; i < 16; i++) {  //2. olan kisinin dizideki konumunu bulur
+            if (scores[i] == min) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    // ucuncu kosucunun arraydeki yerini donduren fonksiyon
+    public int third(Marathon marathon) {
+        int[] scores = marathon.getScores();
+        int min = scores[0];
+
+        MarathonManager marathonManager = new MarathonManager();
+        int first = marathonManager.first(marathon);
+        int second = marathonManager.second(marathon);
+
+        for (int i = 0; i < 16; i++) { //3. olan kisinin skorunu bulur
+            if (i == first) {
+                continue;
+            }
+
+            if (i == second) {
+                continue;
+            }
+
+            else if (scores[i] < min)
+                min = scores[i];
+        }
+
+        int i=0;
+        for (i = 0; i < 16; i++) { //3. olan kisinin dizideki konumunu bulur
+            if (scores[i] == min) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    // skorlara gore siniflara dusen ogrenci sayisini bulan fonksiyon
+    public int[] studentPerScore(Marathon marathon) {
+        int[] numbers = { 0, 0, 0 };
+        int[] scores = marathon.getScores();
+        int temp = 0;
+
+        for (int score : scores) {
+            temp = score / 100;
+            numbers[temp - 2]++;
+        }
+        return numbers;
     }
 }
